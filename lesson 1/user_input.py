be1c6d5d1f1191ec1369f0c69d372e2b923e8e36
annanodiyo <annanodiyo@gmail.com>
2024-03-05 22:40:55 +0300

    first commit

---
+++ lesson 1/user_input.py
@@ -0,0 +1,8 @@
+name = input('What is your name? ')
+
+age = input('how old are you? ')
+
+location = input('where are you? ')
+
+
+print('Hello, ' + name + ' you are ' + age + ' years old  and you are from ' + location)
